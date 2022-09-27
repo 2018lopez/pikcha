e88c37ce16a158d924a92aede5db9cedb58caba2
Imer Lopez <2018119251@ub.edu.bz>
2022-09-27 17:06:01 -0600

    pikcha app and v1

---
+++ src/Pikcha.elm
@@ -0,0 +1,8 @@
+module Pikcha exposing (main)
+
+import Html exposing (Html, div, text, h1)
+import Html.Attributes exposing(class)
+
+main : Html msg
+main =
+        div[ class "header"] [ h1 [] [text "Pikcha" ] ]
