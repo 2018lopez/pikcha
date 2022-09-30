09d7180282006801c5e1eb58795557a7b396e1b9
Imer Lopez <2018119251@ub.edu.bz>
2022-09-30 15:55:45 -0600

    added model, update, update main

--- src/Pikcha.elm
+++ src/Pikcha.elm
@@ -1,8 +1,76 @@
 module Pikcha exposing (main)
 
-import Html exposing (Html, div, text, h1)
-import Html.Attributes exposing(class)
+import Html exposing (..)
+import Html.Attributes exposing(class, src)
+import Html.Events exposing (onClick)
+import Browser
 
-main : Html msg
+type Msg = 
+        Like | Unlike
+baseUrl : String
+baseUrl = 
+        "images/"
+
+initialModel : { url : String, caption : String, liked : Bool } 
+initialModel =
+        {
+                url = baseUrl ++ "cats.jpg"
+                ,caption = "Cat Family"
+                ,liked = False
+        }
+
+
+
+viewPhoto : { url : String, caption : String, liked : Bool }-> Html Msg
+viewPhoto model =
+        let
+              buttonType =
+                if model.liked then "favorite" else "favorite_border"
+              msg =
+                if model.liked then Unlike else Like
+        in
+        
+        div [ class "detailed-photo" ]
+                [
+                        img [ src model.url ] []
+                        ,div [ class " photo-info" ]
+                        [ div [ class "favorite-button" ] 
+                            [ span
+                                [ class "material-icons md-48", onClick msg ]
+                                [ text buttonType ]
+
+                            ]    
+                        ]
+                        , h2 [ class "caption"] [ text model.caption ] 
+
+                ]
+
+view : { url : String, caption : String , liked : Bool} -> Html Msg
+view model =
+        div []
+        [
+           div [ class "header" ]
+           [ h1 [] [ text "Pikcha" ] ]
+           ,div [ class "content-flow" ]
+           [
+               viewPhoto model
+           ]
+
+        ]
+
+update : Msg -> { url : String, caption : String , liked : Bool} -> { url : String, caption : String , liked : Bool}
+update msg model =
+        case msg of
+            Like ->
+                { model | liked = True }
+            Unlike ->
+                { model | liked = False }
+
+
+main : Program () { url : String, caption : String , liked : Bool} Msg
 main =
-        div[ class "header"] [ h1 [] [text "Pikcha" ] ]
+        Browser.sandbox{
+                init = initialModel
+                ,view = view
+                ,update = update
+        }
